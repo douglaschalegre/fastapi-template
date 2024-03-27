3c2fc292e5494c70fedfbc092966288e10c408a6
douglaschalegre <douglas.chalegre@gmail.com>
2024-03-27 20:45:47 -0300

    fix: Add example route to controller list

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,7 +1,14 @@
 """Creation of controller list"""
 
 from . import health
+from . import example
 
+routes = [
+    example.router,
+
+    health.router,  # health route must be the last one!
+]
 tags = [
-    *health.tags,
+    *example.tags,
+    *health.tags,  # health tag must be the last one!
 ]
