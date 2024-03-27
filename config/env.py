20ee6c54c9a6cac89a5b27e031173431e84f1841
douglaschalegre <douglas.chalegre@gmail.com>
2024-03-27 20:45:39 -0300

    fix: Update debug mode variable in env.py

--- config/env.py
+++ config/env.py
@@ -13,12 +13,12 @@ application = dict(
     name=os.getenv('APP_NAME') or 'FastAPI Template',
 )
 
-debug_mode = False  # type: ignore
+DEBUG_MODE = False  # type: ignore
 if os.getenv('DEBUG_MODE') is not None:
-    debug_mode = os.getenv('DEBUG_MODE').upper() == "TRUE"  # type:ignore
+    DEBUG_MODE = os.getenv('DEBUG_MODE').upper() == "TRUE"  # type:ignore
     disable_debug_query = os.getenv('DISABLE_DEBUG_QUERY', "FALSE") == "TRUE"
-    if debug_mode:
+    if DEBUG_MODE:
         logging.getLogger().setLevel(logging.INFO)
-    if debug_mode and not disable_debug_query:
+    if DEBUG_MODE and not disable_debug_query:
         logging.basicConfig()
         logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
