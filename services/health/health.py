31bb07acc7d8beda816eb26a0eef571d8a01e879
douglaschalegre <douglas.chalegre@gmail.com>
2024-03-27 15:13:46 -0300

    chore: Add health service for liveness and readiness checks

---
+++ services/health/health.py
@@ -0,0 +1,12 @@
+'''Health service'''
+from domain.schemas.health import health as health_schemas
+
+
+def check_liveness():
+    '''Check liveness'''
+    return health_schemas.LivenessResponse()
+
+
+def check_readiness():
+    '''Check readiness'''
+    return health_schemas.ReadinessResponse()
