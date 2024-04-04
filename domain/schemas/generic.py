7a2e541061986cf61956c7f07494c3d2103ba35a
Douglas Chalegre <douglas.chalegre@gmail.com>
2024-04-03 22:28:32 -0300

    Update generic.py

--- domain/schemas/generic.py
+++ domain/schemas/generic.py
@@ -6,14 +6,15 @@ from utils.camel import to_camel
 
 class GenericSchema(BaseModel):
     """Generic Class for schemas."""
-    class Config:
-        """Schema config class"""
-        alias_generator = to_camel
-        allow_population_by_field_name = True
+    model_config = ConfigDict(
+        alias_generator=to_camel, # type: ignore
+        populate_by_name=True
+    )
 
 
 class TableSchema(GenericSchema):
     """Class for schemas based on database table.."""
+
     model_config = ConfigDict(
         from_attributes=True
     )
